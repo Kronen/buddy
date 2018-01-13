8ed9d9fb7999e2121c915318289c16a15d230d13
Kronen <kronenr@gmail.com>
2018-01-13 23:22:13 +0100

    Added materialize and jquery dependencies

--- src/main/java/com/kronen/buddy/web/controllers/HelloController.java
+++ src/main/java/com/kronen/buddy/web/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.kronen.buddy.controllers;
+package com.kronen.buddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
