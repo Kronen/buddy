321cf1d1b23e4cc728902449d783245e27fe249d
Alberto G. Lagos <kronenr@gmail.com>
2018-01-14 01:31:58 +0100

    Add i18n capabilities to the web skeleton

---
+++ src/main/java/com/kronen/buddy/common/config/I18NConfig.java
@@ -0,0 +1,19 @@
+package com.kronen.buddy.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18NConfig {
+
+    @Bean(name = "messageSource")
+    public ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource() {
+	ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+	messageSource.setBasenames("classpath:i18n/messages");
+	messageSource.setDefaultEncoding("UTF-8");
+	messageSource.setCacheSeconds(1800);
+	return messageSource;
+    }
+
+}
--- src/test/java/com/kronen/buddy/BuddyApplicationTests.java
+++ src/test/java/com/kronen/buddy/BuddyApplicationTests.java
@@ -1,16 +1,28 @@
 package com.kronen.buddy;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kronen.buddy.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BuddyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private I18NService i18NService;
+    
+    @Test
+    public void testMessageByLocaleService() {
+	String expected = "Materialize Starter Template";
+	String messageId = "index.main.callout";
+	String actual = i18NService.getMessage(messageId);
+	assertEquals("The actual and expected String don't match", expected, actual);
+    }
 
 }
