b73cec3b1c869ce22db6f4f956248d7009f8a6c7
Alberto G. Lagos <kronenr@gmail.com>
2018-01-19 21:39:08 +0100

    Added the feedback journey code

--- src/main/java/com/kronen/buddy/web/controllers/ContactController.java
+++ src/main/java/com/kronen/buddy/web/controllers/ContactController.java
@@ -4,8 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kronen.buddy.backend.service.EmailService;
+import com.kronen.buddy.web.domain.frontend.FeedbackBean;
 
 @Controller
 public class ContactController {
@@ -18,4 +23,17 @@ public class ContactController {
     
     @Autowired
     private EmailService emailService;
+    
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contactGet(ModelMap model) {	
+	model.addAttribute(MODEL_FEEDBACK, new FeedbackBean());
+	return CONTACT_US_VIEW_NAME;
+    }
+    
+    @RequestMapping(value = "/contact", method = RequestMethod.POST)
+    public String contactPost(@ModelAttribute(MODEL_FEEDBACK) FeedbackBean feedback, ModelMap model) {	
+	LOG.debug("Feedback bean content {}", feedback);
+	emailService.sendFeedbackEmail(feedback);
+	return CONTACT_US_VIEW_NAME;
+    }
 }
--- src/main/java/com/kronen/buddy/web/domain/frontend/FeedbackBean.java
+++ src/main/java/com/kronen/buddy/web/domain/frontend/FeedbackBean.java
@@ -2,6 +2,8 @@ package com.kronen.buddy.web.domain.frontend;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Component;
+
 public class FeedbackBean implements Serializable {
     
     private static final long serialVersionUID = 1L;
