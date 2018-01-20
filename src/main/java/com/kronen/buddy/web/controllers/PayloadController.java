a688039571cf01114ca481e42de9023d71f8b2de
Alberto G. Lagos <kronenr@gmail.com>
2018-01-20 20:58:51 +0100

    fix route to the payload page

--- src/main/java/com/kronen/buddy/web/controllers/PayloadController.java
+++ src/main/java/com/kronen/buddy/web/controllers/PayloadController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PayloadController {
     
-    private static final String PAYLOAD_VIEW_NAME = "payload";
+    private static final String PAYLOAD_VIEW_NAME = "payload/payload";
     
     @RequestMapping("/payload")
     public String payload() {
