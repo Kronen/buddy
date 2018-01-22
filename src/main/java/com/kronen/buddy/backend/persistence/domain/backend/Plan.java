ed102c8518de55ba7fd1a5df60bb74ac0a585ed2
Alberto G. Lagos <kronenr@gmail.com>
2018-01-22 02:09:23 +0100

    created the jpa entity code for Spring Security

---
+++ src/main/java/com/kronen/buddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,54 @@
+package com.kronen.buddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private long id;
+    
+    private String name;
+    
+    public Plan() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + (int) (id ^ (id >>> 32));
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null || getClass() != obj.getClass())
+	    return false;
+	Plan other = (Plan) obj;
+	return id == other.id;
+    }
+    
+}
