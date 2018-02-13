b706cee4c322feb9d67c25a61bffd9ea591d940c
Kronen <kronenr@gmail.com>
2018-02-14 00:52:08 +0100

    add forgotmypassword email form

--- src/main/java/com/kronen/buddy/common/config/SecurityConfig.java
+++ src/main/java/com/kronen/buddy/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
+import com.kronen.buddy.web.controllers.ForgotMyPasswordController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,57 +20,58 @@ import com.kronen.buddy.backend.service.UserSecurityService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-        
+
     @Autowired
     private Environment env;
-    
+
     @Autowired
     private UserSecurityService userSecurityService;
-    
-    private static final String[] PUBLIC_MATCHERS = { 
-	    "/webjars/**", 
-	    "/css/**", 
-	    "/js/**", 
-	    "/images/**", 
-	    "/",
-	    "/about/**", 
-	    "/contact/**", 
-	    "/error/**/*",
-	    "/h2-console/**"
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+            "/h2-console/**",
+            ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING
     };
-    
+
     private static final String SALT = "rjge@{sdl¡ç-m4hjrie";
-    
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
-	return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
     }
-    
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-	List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-	// Required by h2 console to work
-	if(activeProfiles.contains("dev")) {
-	    http.csrf().disable();
-	    http.headers().frameOptions().disable();
-	}
-	http
-		.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin().loginPage("/login").defaultSuccessUrl("/payload")
-		.failureUrl("/login?error").permitAll()
-		.and()
-		.logout().permitAll();
+        List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        // Required by h2 console to work
+        if(activeProfiles.contains("dev")) {
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+        }
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
     }
-    
+
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-	auth
-		.userDetailsService(userSecurityService)
-		.passwordEncoder(passwordEncoder());	
+        auth
+                .userDetailsService(userSecurityService)
+                .passwordEncoder(passwordEncoder());
     }
-    
+
 }
--- src/test/java/com/kronen/buddy/test/integration/UserRepositoryIntegrationTest.java
+++ src/test/java/com/kronen/buddy/test/integration/UserRepositoryIntegrationTest.java
@@ -26,68 +26,66 @@ import com.kronen.buddy.common.enums.RolesEnum;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryIntegrationTest extends AbstractIntegrationTest {
-    
-    @Rule public TestName testName = new TestName();
-    
+
+    @Rule
+    public TestName testName = new TestName();
+
     @Before
     public void init() {
-	assertNotNull(planRepository);
-	assertNotNull(roleRepository);
-	assertNotNull(userRepository);
+        assertNotNull(planRepository);
+        assertNotNull(roleRepository);
+        assertNotNull(userRepository);
     }
-    
+
     @Test
     public void createNewPlan() {
-	Plan basicPlan = createPlan();
-	planRepository.save(basicPlan);
-	Optional<Plan> retrievedPlan = planRepository.findById(PlansEnum.BASIC.getId());
-	assertThat(retrievedPlan.isPresent()).isTrue();
+        Plan basicPlan = createPlan();
+        planRepository.save(basicPlan);
+        Optional<Plan> retrievedPlan = planRepository.findById(PlansEnum.BASIC.getId());
+        assertThat(retrievedPlan.isPresent()).isTrue();
     }
-    
+
     @Test
     public void createNewRole() {
-	Role basicRole = createRole(RolesEnum.BASIC);
-	roleRepository.save(basicRole);
-	Optional<Role> retrievedRole = roleRepository.findById(RolesEnum.BASIC.getId());
-	assertThat(retrievedRole.isPresent()).isTrue();
+        Role basicRole = createRole(RolesEnum.BASIC);
+        roleRepository.save(basicRole);
+        Optional<Role> retrievedRole = roleRepository.findById(RolesEnum.BASIC.getId());
+        assertThat(retrievedRole.isPresent()).isTrue();
     }
-    
+
     @Test
-    @Transactional
-    public void createNewUser() {	
-	User user = createUser(testName);
-	Optional<User> newlyCreatedUser = userRepository.findById(user.getId());
-	
-	assertThat(newlyCreatedUser.isPresent()).isTrue();
-	assertThat(newlyCreatedUser.get().getId() != 0).isTrue();
-	assertThat(newlyCreatedUser.get().getPlan()).isNotNull();
-	Set<UserRole> newlyCreatedUserUserRoles = newlyCreatedUser.get().getUserRoles();
-	assertThat(newlyCreatedUserUserRoles).isNotNull();
-	for(UserRole ur : newlyCreatedUserUserRoles) {
-	    assertThat(ur.getRole()).isNotNull();
-	    assertThat(ur.getRole().getId()).isNotNull();
-	}
+    public void createNewUser() {
+        User user = createUser(testName);
+        Optional<User> newlyCreatedUser = userRepository.findById(user.getId());
+
+        assertThat(newlyCreatedUser.isPresent()).isTrue();
+        assertThat(newlyCreatedUser.get().getId() != 0).isTrue();
+        assertThat(newlyCreatedUser.get().getPlan()).isNotNull();
+        Set<UserRole> newlyCreatedUserUserRoles = newlyCreatedUser.get().getUserRoles();
+        assertThat(newlyCreatedUserUserRoles).isNotNull();
+        for(UserRole ur : newlyCreatedUserUserRoles) {
+            assertThat(ur.getRole()).isNotNull();
+            assertThat(ur.getRole().getId()).isNotNull();
+        }
     }
-    
+
     @Test
-    @Transactional
-    public void deleteUser() {	
-	User user = createUser(testName);
-	
-	userRepository.delete(user);
-	Optional<User> deletedUser = userRepository.findById(user.getId());
-	
-	assertThat(deletedUser.isPresent()).isFalse();
+    public void deleteUser() {
+        User user = createUser(testName);
+
+        userRepository.delete(user);
+        Optional<User> deletedUser = userRepository.findById(user.getId());
+
+        assertThat(deletedUser.isPresent()).isFalse();
     }
-    
+
     @Test
-    @Transactional
-    public void getUserByEmail() {	
-	User user = createUser(testName);
-	
-	Optional<User> foundUser = userRepository.findByEmail(user.getEmail());
-	assertThat(foundUser.isPresent()).isTrue();
-	assertThat(foundUser.get().getId()).isNotNull();
+    public void getUserByEmail() {
+        User user = createUser(testName);
+
+        Optional<User> foundUser = userRepository.findByEmail(user.getEmail());
+        assertThat(foundUser.isPresent()).isTrue();
+        assertThat(foundUser.get().getId()).isNotNull();
     }
-    
+
 }
