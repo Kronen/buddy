7ecf7250da43732d69f3f0324983a3dc0a2c371d
Kronen <kronenr@gmail.com>
2018-02-26 19:31:46 +0100

    changed styles

--- src/main/java/com/kronen/buddy/web/controllers/ForgotMyPasswordController.java
+++ src/main/java/com/kronen/buddy/web/controllers/ForgotMyPasswordController.java
@@ -1,10 +1,13 @@
 package com.kronen.buddy.web.controllers;
 
+import com.kronen.buddy.backend.persistence.domain.backend.PasswordResetToken;
+import com.kronen.buddy.backend.service.PasswordResetTokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ForgotMyPasswordController {
@@ -14,8 +17,18 @@ public class ForgotMyPasswordController {
     private static final String EMAIL_ADDRESS_VIEW_NAME = "forgotmypassword/emailForm";
     public static final String FORGOT_PASSWORD_URL_MAPPING = "/forgotmypassword";
 
+    @Autowired
+    private PasswordResetTokenService passwordResetTokenService;
+
     @RequestMapping(value = FORGOT_PASSWORD_URL_MAPPING, method = RequestMethod.GET)
-    public String forgotPassword() {
+    public String forgotPasswordGet() {
+        return EMAIL_ADDRESS_VIEW_NAME;
+    }
+
+    @RequestMapping(value = FORGOT_PASSWORD_URL_MAPPING, method = RequestMethod.POST)
+    public String forgotPasswordPost(@RequestParam("email") String email, ModelMap model) {
+        PasswordResetToken passwordResetToken = passwordResetTokenService
+                .
         return EMAIL_ADDRESS_VIEW_NAME;
     }
 
